ee7620343e4fe341826fd06f67137d8995cc8014
IRAKOZE Nsumba Herve <nsumbaherve11@gmail.com>
2026-04-21 18:10:40 +0200

    Second Commit

--- src/main/java/rw/rab/cropDao/CropDao.java
+++ src/main/java/rw/rab/cropDao/CropDao.java
@@ -4,10 +4,48 @@
  */
 package rw.rab.cropDao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import rw.rab.model.Crop;
+
 /**
  *
  * @author nsumba
  */
 public class CropDao {
+    //JDBC Properties
+    private String jdbcUrl = "jdbc:postgresql://localhost:5432/crop_management_java";
+    private String dbUsername = "postgres";
+    private String dbPassword = "1234";
+    
+    //CREATE OPERATION
+    public Integer registerCrop(Crop cropObj){
+        //step 0: surround everything with try and catch
+        try{
+            //step1: create a connection
+            Connection con = DriverManager.getConnection(jdbcUrl, dbUsername, dbPassword);
+            //step 2: create statement
+            String sql = "INSERT INTO crops (name, id, lifespan, price) VALUES(?, ?, ?, ?)";
+            PreparedStatement pst = con.prepareStatement(sql);
+            
+            pst.setString(1, cropObj.getName());
+            pst.setInt(2, cropObj.getId());
+            pst.setInt(3, cropObj.getLifespan());
+            pst.setInt(4, cropObj.getPrice());
+            
+            //step3: executing the data into the database
+            int rowsAffected = pst.executeUpdate();
+            
+            //step4: close connection
+            con.close();
+            return rowsAffected;
+            
+        }catch(Exception ex){
+            ex.printStackTrace();
+            return 0;
+        }
+    }
+    
     
 }
